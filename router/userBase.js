9f49bb8cad8d31e0674da6c77edfbab91921d162
LedgerNinja0x <sweetheart199558@gmail.com>
2024-10-24 14:42:44 -0700

    update backend

--- router/userBase.js
+++ router/userBase.js
@@ -609,7 +609,7 @@ export async function gameHandler(req) {
       }
     }
     const userBalance = checkBalance(inputData.userId, inputData.bet, inputData.isReal);
-    if (userBalance < 0) {
+    if (userBalance <= 0) {
       return {
         status: "error",
         data: {
