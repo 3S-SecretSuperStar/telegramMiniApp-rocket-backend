f862728fa1fc7b9930005d6a81c0817896b62e47
DeaneBigay <deanebigay89@outlook.com>
2024-11-02 00:05:51 -0700

    update using url

--- router/userBase.js
+++ router/userBase.js
@@ -674,56 +674,38 @@ export async function gameHandler(req) {
 }
 
 
-// Set The Storage Engine
-const storage = multer.diskStorage({
-  destination: '/var/avatar/icon/',
-  filename: function(req, file, cb){
-    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-
-// Init Upload
-const upload = multer({
-  storage: storage,
-  limits:{fileSize: 1000000},
-  fileFilter: function(req, file, cb){
-    checkFileType(file, cb);
-  }
-}).single('myImage');
-
-// Check File Type
-function checkFileType(file, cb){
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
-
-  if(mimetype && extname){
-    return cb(null,true);
-  } else {
-    cb('Error: Images Only!');
-  }
+export function uploadIcon(req){
+  const data = req.body;
+  const uploadStatus = saveIcon(data.fileUrl);
+  
 }
-
-export function uploadIcon(req,res){
-  upload(req, res, (err) => {
-    if(err){
-      res.render('index', {
-        msg: err
+async function saveIcon(imageUrl){
+  console.log(imageUrl)
+  const realUrl = imageUrl.slice(4)
+  if (imageUrl) {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: avatarImg,
+        responseType: 'stream'
       });
-    } else {
-      if(req.file == undefined){
-        res.render('index', {
-          msg: 'Error: No File Selected!'
-        });
-      } else {
-        res.render('index', {
-          msg: 'File Uploaded!',
-          file: `uploads/${req.file.filename}`
-        });
-      }
+
+      // console.log("response data: ",response.data)
+      const savePath = "/var/avatar/icon" + "icon".toString() + '.jpg';
+      console.log("save path : ", savePath)
+      const writer = fs.createWriteStream(savePath);
+      await response.data.pipe(writer);
+      writer.on('finish', () => {
+        console.log("Finish all")
+      })
+      writer.on('error', () => {
+        console.log('error this url', error)
+      })
+      console.log("user String", userId.toString())
+      return `https://telegramminiapp-rocket-backend-lbyg.onrender.com/icon/${userId.toString()}.jpg`
+    } catch (error) {
+      console.log(error)
     }
-  });
+  }
+  else return null
 }
