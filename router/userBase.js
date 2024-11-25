77145c85e4a19839d839546ee8d6e0ed100e4d3e
3S-SecretSuperStar <juliosantos95617@gmail.com>
2024-11-25 09:39:47 -0700

    add task ads remve peform

--- router/userBase.js
+++ router/userBase.js
@@ -592,8 +592,8 @@ export async function performDailyADS(req) {
 
     const currentDate = moment().utc().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
     const performDailyReward = req.body.isReal
-      ? await db.collection('users').updateOne({ user_id: req.body.userId }, { $set: { 'dailyHistory': currentDate }, $inc: { 'balance.real': parseFloat(req.body.amount) }, $pull: { "task.real.achieve_task": 34 } })
-      : await db.collection('users').updateOne({ user_id: req.body.userId }, { $set: { 'dailyHistory': currentDate }, $inc: { 'balance.virtual': parseFloat(req.body.amount) }, $pull: { "task.virtual.achieve_task": 34 } })
+      ? await db.collection('users').updateOne({ user_id: req.body.userId }, { $set: { 'dailyADS': currentDate }, $inc: { 'balance.real': parseFloat(req.body.amount) }, $pull: { "task.real.achieve_task": 34 } })
+      : await db.collection('users').updateOne({ user_id: req.body.userId }, { $set: { 'dailyADS': currentDate }, $inc: { 'balance.virtual': parseFloat(req.body.amount) }, $pull: { "task.virtual.achieve_task": 34 } })
     // console.log("performDailyReward",performDailyReward)
   } catch (error) {
     console.log(error)
