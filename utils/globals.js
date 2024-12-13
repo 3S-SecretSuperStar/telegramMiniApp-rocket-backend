dce133226565ed07bf7c8d241d8d5da693c201ca
LedgerNinja0x <sweetheart199558@gmail.com>
2024-12-13 10:21:31 -0700

    fix: bot toke update

--- utils/globals.js
+++ utils/globals.js
@@ -29,4 +29,4 @@ export const TASK_LIST =[
   {method : TASK_TYPE[1], limit : 5, value :300}
 ]
 
-export const BOT_TOKEN = "7379750890:AAGYFlyXnjrC8kbyxRdYhUbisoTbCWdPCg8";
+export const BOT_TOKEN = "8051822474:AAHjS5hg44-BlAX0K05eBM4CRuFumA9-Wlo";
